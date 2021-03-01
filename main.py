da41df1b1f9cfb5ca5699125abd2270a8f71c6f9
nxblnd <52958704+nxblnd@users.noreply.github.com>
2021-03-01 23:58:15 +0300

    Simple bot, can reply to /start and /help messages

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import os
+from aiogram import Bot, Dispatcher, executor, types
+
+bot = Bot(os.getenv('BOT_TOKEN'))
+dispatcher = Dispatcher(bot)
+
+
 def main():
-    pass
+    executor.start_polling(dispatcher, skip_updates=True)
+
+
+@dispatcher.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.message):
+    await message.answer("I'm simple weather bot. Powered by OpenWeatherMap data.\n"
+                         "Type /help to get this message again.")
 
 
 if __name__ == '__main__':
