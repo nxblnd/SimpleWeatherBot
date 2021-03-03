a8121a9802fcb4ec24bf6fb4639e948e34741af4
nxblnd <52958704+nxblnd@users.noreply.github.com>
2021-03-03 19:05:41 +0300

    Somewhat working requests to OneCall API

--- OwmRequests.py
+++ OwmRequests.py
@@ -6,7 +6,8 @@ import requests
 from OwmExceptions import OwmNoResponse, OwmLocationException
 
 OWM_links = {'current': 'https://api.openweathermap.org/data/2.5/weather',
-             'geocoding': 'http://api.openweathermap.org/geo/1.0/direct'}
+             'geocoding': 'http://api.openweathermap.org/geo/1.0/direct',
+             'onecall': 'https://api.openweathermap.org/data/2.5/onecall'}
 
 
 async def get_weather(lat: float, lon: float) -> dict[str, Any]:
@@ -29,3 +30,14 @@ async def get_city_coords(city: str) -> dict[str, float]:
     if not geodata:
         raise OwmLocationException('City not found')
     return {'lat': geodata[0]['lat'], 'lon': geodata[0]['lon']}
+
+
+async def get_all_weather(lat: float, lon: float) -> dict[str, float]:
+    r = requests.get(OWM_links['onecall'], params={'lat': lat,
+                                                   'lon': lon,
+                                                   'appid': os.getenv('OWM_TOKEN'),
+                                                   'units': 'metric',
+                                                   'exclude': ['minutely', 'daily']})
+    if r.status_code != requests.codes.OK:
+        raise OwmNoResponse
+    return r.json()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
+import time
 from typing import Any
 
 from aiogram import Bot, Dispatcher, executor, types
 
 from OwmExceptions import OwmNoResponse, OwmLocationException
-from OwmRequests import get_weather, get_city_coords
+from OwmRequests import get_weather, get_city_coords, get_all_weather
 
 bot = Bot(os.getenv('BOT_TOKEN'))
 dispatcher = Dispatcher(bot)
@@ -34,13 +35,43 @@ async def send_current_weather(message: types.message):
         await message.answer(build_current_weather_msg(weather))
 
 
+@dispatcher.message_handler(commands='onecall')
+async def send_onecall_weather(message: types.message):
+    _, city = message.get_full_command()
+    try:
+        coords = await get_city_coords(city)
+        weather = await get_all_weather(coords['lat'], coords['lon'])
+    except OwmLocationException:
+        await message.answer('This location could not be found in OpenWeatherMap database')
+    except OwmNoResponse:
+        await message.answer("No connection to OpenWeatherMap")
+    else:
+        await message.answer(build_onecall_msg(weather))
+
+
 def build_current_weather_msg(weather: dict[str, Any]) -> str:
     return f"Current weather in {weather['name']} is {weather['weather'][0]['main']}\n" \
-           f"Temperature is {round(weather['main']['temp'])}℃ (feels like {round(weather['main']['feels_like'])}℃)\n" \
+           f"Temperature is {round(weather['main']['temp'])}℃ " \
+           f"(feels like {round(weather['main']['feels_like'])}℃)\n" \
            f"Atmospheric pressure is {weather['main']['pressure']} kPa\n" \
            f"Air humidity is {weather['main']['humidity']}%\n" \
            f"Wind direction is {weather['wind']['deg']}° with {weather['wind']['speed']} m/s speed\n" \
            f"Cloudiness is {weather['clouds']['all']}%"
+
+
+def build_onecall_msg(weather: dict[str, Any]) -> str:
+    return f"Current weather at lat {weather['lat']}, lon {weather['lon']} is {weather['current']['weather'][0]['main']}\n" \
+           f"Temperature is {round(weather['current']['temp'])}℃ " \
+           f"(feels like {round(weather['current']['feels_like'])}℃)\n" \
+           f"Atmospheric pressure is {weather['current']['pressure']} kPa\n" \
+           f"Air humidity is {weather['current']['humidity']}%\n" \
+           f"Wind direction is {weather['current']['wind_deg']}° with {weather['current']['wind_speed']} m/s speed\n" \
+           f"Cloudiness is {weather['current']['clouds']}%\n\n" \
+           f"In next 12 hours:\n" + \
+           ''.join(f"• {time.strftime('%H:00', time.gmtime(hour['dt'] + weather['timezone_offset']))} "
+                   f"{hour['weather'][0]['main']},\n"
+                   f"  {round(hour['temp'])}℃ (feels like {round(hour['feels_like'])}℃).\n"
+                   f"  Probability of precipitation {round(hour['pop'] * 100)}%\n" for hour in weather['hourly'][:12])
 
 
 @dispatcher.message_handler(lambda message: message.is_command())
